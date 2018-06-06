6d87313e5303c7ed1d84ffa2151ac1203bb785b6
Brennen Berkley <hello@brennenberkley.com>
2018-06-05 23:58:47 -0600

    Moves are now validated for rooks, queens, and bishops

--- movevalidator/movevalidator.py
+++ movevalidator/movevalidator.py
@@ -60,7 +60,7 @@ class MoveValidator():
 		elif piece == "N":
 			return self.__move_knight(file, rank, capture, origin_file, origin_rank)
 		elif piece == "B":
-			return self.__move_biship(file, rank, capture, origin_file, origin_rank)
+			return self.__move_bishop(file, rank, capture, origin_file, origin_rank)
 		elif piece == "K":
 			return self.__move_king(file, rank, capture, origin_file, origin_rank)
 		elif piece == "Q":
@@ -74,16 +74,12 @@ class MoveValidator():
 			return str(int(move_number)) + "."
 
 	def get_color_to_move(self):
-		return "b" if self.__current_move % 2 == 0 else "w"
+		return "black" if self.__current_move % 2 == 0 else "white"
 
-	def __move_pawn(self, file, rank, capture, origin_file):
-		new_rank = self.__get_index(rank)
-		new_file = self.__get_index(file)
-
-		f = self.__get_index(file)
-		r = self.__get_index(rank)
-		files = None
-		ranks = None
+	def __move_pawn(self, new_file, new_rank, capture, origin_file):
+		rank = self.__get_index(new_rank)
+		file = self.__get_index(new_file)
+		squares = []
 
 		color = self.get_color_to_move()
 		# Determine valid origin squares
@@ -91,35 +87,194 @@ class MoveValidator():
 			if (origin_file is None):
 				return False
 
-			files = [self.__get_index(origin_file)]
-			if color == 'w':
-				ranks = [r - 1]
+			if color == 'white':
+				squares.append([rank - 1, self.__get_index(origin_file)])
 			else:
-				ranks = [r + 1]
+				squares.append([rank + 1, self.__get_index(origin_file)])
 		else:
-			files = [f]
-			if color == 'w':
-				ranks = [r - 1]
-				if r == 3:
-					ranks.append(r - 2)
+			if color == 'white':
+				squares.append([rank - 1, file])
+				if rank == 3:
+					squares.append([rank - 2, file])
 			else:
-				ranks = [r + 1]
-				if r == 4:
-					ranks.append(r + 2)
+				squares.append([rank + 1, file])
+				if rank == 4:
+					squares.append([rank + 2, file])
 
 		# Try each origin square
-		for f in files:
-			for r in ranks:
-				if f < 0 or r < 0 or f > 7 or r > 7:
-					continue
-				if not self.__open_path(f, r, new_file, new_rank):
-					continue
+		for square in squares:
+			r = square[0]
+			f = square[1]
+			if f < 0 or r < 0 or f > 7 or r > 7:
+				continue
+			if not self.__open_path(f, r, file, rank):
+				continue
 
-				if self.__board_position[r][f] == color + "p":
-					self.__update_board(f, r, new_file, new_rank)
-					return True
+			if self.__board_position[r][f] == color[0] + "p":
+				self.__update_board(f, r, file, rank)
+				return True
 
 		return False
+
+	def __move_knight(self, new_file, new_rank, capture, origin_file, origin_rank):
+		return False
+
+
+	def __move_rook(self, new_file, new_rank, capture, origin_file, origin_rank):
+		rank = self.__get_index(new_rank)
+		file = self.__get_index(new_file)
+		squares = []
+		color = self.get_color_to_move()
+
+		# Determine valid origin squares
+		for offset in range(-7, 8):
+			squares.append([rank, file + offset])
+			squares.append([rank + offset, file])
+
+		# Try each origin square
+		for square in squares:
+			r = square[0]
+			f = square[1]
+			if f < 0 or r < 0 or f > 7 or r > 7:
+				continue
+			if not self.__open_path(f, r, file, rank):
+				continue
+
+			if self.__board_position[r][f] == color[0] + "R":
+				self.__update_board(f, r, file, rank)
+				return True
+
+		return False
+
+
+	def __move_bishop(self, new_file, new_rank, capture, origin_file, origin_rank):
+		rank = self.__get_index(new_rank)
+		file = self.__get_index(new_file)
+		squares = []
+		color = self.get_color_to_move()
+
+		# Determine valid origin squares
+		for offset in range(-7, 8):
+			squares.append([rank + offset, file + offset])
+			squares.append([rank + offset, file - offset])
+
+		# Try each origin square
+		for square in squares:
+			r = square[0]
+			f = square[1]
+			if f < 0 or r < 0 or f > 7 or r > 7:
+				continue
+			if not self.__open_path(f, r, file, rank):
+				continue
+
+			if self.__board_position[r][f] == color[0] + "B":
+				self.__update_board(f, r, file, rank)
+				return True
+
+		return False
+
+
+	def __move_king(self, file, rank, capture, origin_file, origin_rank):
+		return False
+
+
+	def __move_queen(self, new_file, new_rank, capture, origin_file, origin_rank):
+		rank = self.__get_index(new_rank)
+		file = self.__get_index(new_file)
+		squares = []
+		color = self.get_color_to_move()
+
+		# Determine valid origin squares
+		for offset in range(-7, 8):
+			squares.append([rank, file + offset])
+			squares.append([rank + offset, file])
+			squares.append([rank + offset, file + offset])
+			squares.append([rank + offset, file - offset])
+
+		# Try each origin square
+		for square in squares:
+			r = square[0]
+			f = square[1]
+			if f < 0 or r < 0 or f > 7 or r > 7:
+				continue
+			if not self.__open_path(f, r, file, rank):
+				continue
+
+			if self.__board_position[r][f] == color[0] + "Q":
+				self.__update_board(f, r, file, rank)
+				return True
+
+		return False
+
+	def __get_index(self, value):
+		if value in "abcdefgh":
+			return "abcdefgh".find(value)
+		else:
+			return "12345678".find(value)
+
+	def __open_path(self, f1, r1, f2, r2):
+		if f1 == f2:
+			# vertical movement
+			if r1 < r2:
+				for rank in range(r1 + 1,r2):
+					if self.__board_position[rank][f1] is not None:
+						return False	
+			else:
+				for rank in range(r2 + 1,r1):
+					if self.__board_position[rank][f1] is not None:
+						return False
+
+		elif r1 == r2:
+			# horizontal movement
+			if r1 < r2:
+				for file in range(f1 + 1,f2):
+					if self.__board_position[r1][file] is not None:
+						return False
+			else:
+				for file in range(f2 + 1,f1):
+					if self.__board_position[r1][file] is not None:
+						return False
+
+		else:
+			# diagonal movement
+			if abs(r1 - r2) != abs(f1 - f2):
+				print("Error: Not a diagonal")
+				return False
+
+			if (f1 < f2):
+				for offset in range(1, f2 - f1):
+					if (r1 < r2):
+						if self.__board_position[r1 + offset][f1 + offset] is not None:
+							return False
+					else:
+						if self.__board_position[r1 - offset][f1 + offset] is not None:
+							return False
+			else: 
+				for offset in range(1, f1 - f2):
+					if (r1 < r2):
+						if self.__board_position[r1 + offset][f2 + offset] is not None:
+							return False
+					else:
+						if self.__board_position[r1 - offset][f2 + offset] is not None:
+							return False
+
+		return True
+
+	def __update_board(self, f1, r1, f2, r2):
+		self.__board_position[r2][f2] = self.__board_position[r1][f1]
+		self.__board_position[r1][f1] = None
+		self.__current_move += 1
+
+
+
+
+
+
+
+
+
+
+
 
 	def __move_pawn_old(self, file, rank, capture, o_file):
 		new_rank = self.__get_index(rank)
@@ -178,80 +333,6 @@ class MoveValidator():
 
 		self.__update_board(origin_file, origin_rank, new_file, new_rank)
 		return True
-
-	def __move_knight(self, file, rank, capture, origin_file, origin_rank):
-		return False
-
-
-	def __move_rook(self, file, rank, capture, origin_file, origin_rank):
-		return False
-
-
-	def __move_bishop(self, file, rank, capture, origin_file, origin_rank):
-		return False
-
-
-	def __move_king(self, file, rank, capture, origin_file, origin_rank):
-		return False
-
-
-	def __move_queen(self, file, rank, capture, origin_file, origin_rank):
-		return False
-
-	def __get_index(self, value):
-		if value in "abcdefgh":
-			return "abcdefgh".find(value)
-		else:
-			return "12345678".find(value)
-
-	def __open_path(self, f1, r1, f2, r2):
-		if f1 == f2:
-			# vertical movement
-			if r1 < r2:
-				for rank in range(r1 + 1,r2):
-					if self.__board_position[rank][f1] is not None:
-						return False	
-			else:
-				for rank in range(r2 + 1,r1):
-					if self.__board_position[rank][f1] is not None:
-						return False
-
-		elif r1 == r2:
-			# horizontal movement
-			if r1 < r2:
-				for file in range(f1 + 1,f2):
-					if self.__board_position[r1][file] is not None:
-						return False
-			else:
-				for file in range(f2 + 1,f1):
-					if self.__board_position[r1][file] is not None:
-						return False
-
-		else:
-			# diagonal movement
-			if abs(r1 - r2) != abs(f1 - f2):
-				print("Error: Not a diagonal")
-				return False
-
-			smallerf = f1 if f1 < f2 else f2
-			largerf = f1 if f1 > f2 else f2
-			smallerr = r1 if r1 < r2 else r2
-
-			for f in range(smallerf, largerf):
-				if self.__board_position[smallerr + (f - smallerf)][f] is not None:
-					return False
-
-		return True
-
-	def __update_board(self, f1, r1, f2, r2):
-		self.__board_position[r2][f2] = self.__board_position[r1][f1]
-		self.__board_position[r1][f1] = None
-		self.__current_move += 1
-
-
-
-
-
 
 
 
